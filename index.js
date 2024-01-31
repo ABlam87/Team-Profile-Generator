a3f935ebf681bd20ef0fc80dfe8774fc69c35157
ABlam87 <adamblampied@gmail.com>
2024-01-31 11:30:46 +0000

    added all classes w properties and methods

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const employees = [];
+
+//PseudoCode
+// Manager Info
+// Initial Prompt w/ choices
+//1 - add intern (new function, ending with Initial Prompt being called)
+//2 - add engineer (new function, ending with Initial Prompt being called)
+//3 - finish building team - render HTML
+// constructors for each role, base being employee, and all others importing then extending employee
+
+
